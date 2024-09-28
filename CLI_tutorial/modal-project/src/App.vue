69825752c3f1e9ac3ad19204d6ffca2b8a2a2f02
Joshua O. Tolentino <86734067+Murasakinosora@users.noreply.github.com>
2024-09-28 08:12:24 +0800

    ep5 stopped before the challenge solution, managed to solve it but will follow the solution on the video later

--- CLI_tutorial/modal-project/src/App.vue
+++ CLI_tutorial/modal-project/src/App.vue
@@ -4,7 +4,30 @@
     <h1>{{ title }}</h1>
     <input type="text" ref="name" />
     <button @click="handleClick">click me</button>
-    <modal />
+
+    <div v-if="showModal">
+      <Modal
+        :header="header"
+        :text="text"
+        :theme="theme"
+        @close="modalClick"
+        @tap="handleSecond"
+      >
+        <!--changed modal format to use slots-->
+        <h1>Giveaway</h1>
+        <p>using slots</p>
+        <!--creating a named slot-->
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+      </Modal>
+      <!--receiving the emitted event listener on @close-->
+    </div>
+
+    <button @click="modalClick">Show Modal</button>
+    <!---header is a prop-->
+    <!---you can attribute bind a prop to cycle through passed value, attribute binding also allow to pass value from the return function below-->
     <!--to call a component imported on the script tag do <Name />-->
   </div>
 </template>
@@ -18,20 +41,28 @@ example
 To reference the input field on the event handler use this.$refs.refName
 -->
 <script>
-import modal from "./components/modal.vue";
+import Modal from "./components/modal.vue";
 //export is the same with the app.js which contained data,methods, and computed
 export default {
   name: "App",
-  components: { modal },
+  components: { Modal },
   data() {
     return {
       title: "First App",
+      header: "Sign up for a giveaway",
+      text: "hello",
+      theme: "sale",
+      showModal: false,
+      showModalTwo: false,
     };
   },
   methods: {
     handleClick() {
       this.$refs.name.classList.add("active"); //gets a hold of the element in the dom with the ref named "name" and adds a class to it using classList.add("") method
       this.$refs.name.focus(); // puts the focus inside the input field, in which they can type directly after clicking the button
+    },
+    modalClick() {
+      this.showModal = !this.showModal;
     },
   },
 };
--- CLI_tutorial/modal-project/src/components/modal.vue
+++ CLI_tutorial/modal-project/src/components/modal.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
+  <div class="backdrop" @click.self="closeModal">
+    <!--event modifier syntax: @click.[choose event modifier] in this case @click.self only fires the event listener when the exact element is clicked-->
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!--class sale will be assigned if theme === 'sale'-->
+      <!--conditional classes allows you to create various themes-->
+
+      <!--Passed header value from modal element on App.vue-->
+      <h1>{{ header }}</h1>
       <p>modal content</p>
+      <!--renders the passed value on the modal element found inside app.vue-->
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+        <!--renders the named slot with the name of "links"-->
+      </div>
     </div>
   </div>
 </template>
 
+
+<script>
+//creating prop for header that is passed on the modal found on App.vue
+//props are reusable as one source of truth
+export default {
+  props: {
+    header: String,
+    text: String,
+    theme: String,
+    showbutton: {
+      type: Boolean,
+      default: true, // Button will be visible by default unless specified
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close"); // custom event emits an event listener that can be listed to on the parent component which is App.vue by assigning @close on the modal element
+    },
+  },
+};
+</script>
+
+
 <style>
+/** scoped makes the style only affect the elements on this component but having specific selector is still recommended*/
 .backdrop {
   top: 0;
   position: fixed;
@@ -21,5 +57,38 @@
   margin: 100px auto;
   background: white;
   border-radius: 10px;
+}
+
+.modal p,
+.modal h1 {
+  color: rgb(142, 184, 163);
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
+}
+
+.modal.actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  position: relative;
+}
+
+.modal .actions a {
+  color: white;
+  padding: 8px;
+  border: 1px solid white;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.actions button {
+  z-index: 1000;
+  position: relative;
 }
 </style>
