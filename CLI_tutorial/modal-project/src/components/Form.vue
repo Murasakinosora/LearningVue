ce41ca7eb46d6f0156353657a20d66a1014b7ba7
Joshua O. Tolentino <86734067+Murasakinosora@users.noreply.github.com>
2024-09-30 01:37:01 +0800

    Intro to forms
    Used filter to remove clicked skills on the form
    
    dynamic display of added skills using keyup keyboard event

--- CLI_tutorial/modal-project/src/components/Form.vue
+++ CLI_tutorial/modal-project/src/components/Form.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <form>
+    <!-- @submit.prevent does not refresh the page-->
+    <form @submit.prevent="handleSubmit">
       <h1>Form & inputs</h1>
       <label>Email</label>
       <input
@@ -11,11 +12,9 @@
       <!--v model tracks the inputs and assigns it to the variable on data function in real-time-->
 
       <label>Password</label>
-      <input
-        type="password"
-        required
-        v-model="password"
-      /><!--two way data binding using vmodel-->
+      <input type="password" required v-model="password" />
+      <div v-if="passwordErr">{{ passwordErr }}</div>
+      <!--two way data binding using vmodel-->
 
       <label>Role:</label>
       <select v-model="role">
@@ -23,13 +22,21 @@
         <option value="designer">Web Designer</option>
       </select>
 
+      <!---Keyboard Events-->
+      <label>Add Skills [Press Space After Each Skill]</label>
+      <!--keyboard event that fires the function when the pressed key has been let go-->
+      <input type="text" v-model="tempskill" @keyup.space="addSkill" />
+      <!--needs a unique key to cycle through the array, attribute binded the skill since its unique-->
+      <div v-for="skill in skills" :key="skill" class="pill">
+        <p @click="dels(skill)">{{ skill }}</p>
+      </div>
+
       <!--two way data binding using vmodel on checkboxes-->
       <div class="terms">
         <input type="checkbox" required v-model="terms" />
         <label>Accept Terms and Conditions</label>
       </div>
-
-      <!--other way to work with checkboxes and store checked names in one array in data-->
+      <!--other way to work with checkboxes and store checked names in one array in data
       <div>
         <div>
           <input type="checkbox" value="shaun" v-model="names" />
@@ -46,8 +53,10 @@
           <label>mario</label>
         </div>
       </div>
-
-      <!---Keyboard Events-->
+    -->
+      <div class="submit">
+        <button>Create an Account</button>
+      </div>
     </form>
   </div>
 </template>
@@ -61,7 +70,40 @@ export default {
       role: "", // tracks the value of the selected role on the select box
       terms: false,
       names: [],
+      tempskill: "",
+      skills: [],
+      passwordErr: "",
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (this.tempskill) {
+        //validates if the entry is a duplicate or not
+        if (this.skills.includes(this.tempskill)) {
+          this.tempskill = "";
+        } else {
+          this.skills.push(this.tempskill);
+          this.tempskill = "";
+        }
+      }
+    },
+    dels(skill) {
+      //uses filter function to update skills property
+      this.skills = this.skills.filter((item) => {
+        //returns true if not equal to the clicked item thus remains on the array
+        return skill !== item;
+      });
+    },
+    handleSubmit() {
+      //validation of password
+      this.passwordErr =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 characters long"; // ternary operator // if the expression is true the left part after the ? will be assigned
+      if (!this.passwordErr) {
+        console.log(this.email);
+      }
+    },
   },
 };
 </script>
@@ -114,5 +156,32 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+
+.pill {
+  display: inline-block;
+  border-radius: 20px;
+  background: #470857;
+  color: white;
+  padding: 6px 12px;
+  border: 1px solid black;
+  margin: 20px 10px 0 0;
+  cursor: pointer;
+  letter-spacing: 1px;
+}
+.pill p {
+  margin: 0 0;
+}
+.submit {
+  margin: 40px auto;
+  text-align: center; /** centers the button// content of the div */
+}
+.submit button {
+  width: 75%;
+  background: #90559e;
+  color: white;
+  border-radius: 20px;
+  padding: 10px 0;
+  border: none;
 }
 </style>
