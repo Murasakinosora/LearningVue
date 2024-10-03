67c8692186968b88243995d35a8ee2bcccb2c864
Joshua O. Tolentino <86734067+Murasakinosora@users.noreply.github.com>
2024-10-03 08:40:04 +0800

    installed json server
    fetched data from an example json file for jobs

--- CLI_tutorial/modal-project/src/views/jobs/Jobs.vue
+++ CLI_tutorial/modal-project/src/views/jobs/Jobs.vue
@@ -13,6 +13,23 @@
         <h1>{{ job.title }}</h1>
       </router-link>
     </div>
+
+    <!--fetching data from json-->
+    <p>Fetching from JSON</p>
+
+    <div v-for="js in json" :key="js.id">
+      <!--Adding params on the attributed binded to allows us to direct to the right link/rout eg /job/[id]-->
+      <!---->
+      <router-link
+        :to="{
+          name: 'JobDetails',
+          params: { id: js.id, description: js.description },
+        }"
+        class="job"
+      >
+        <h1>{{ js.title }}</h1>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -37,7 +54,20 @@ export default {
           description: "lorem3",
         },
       ],
+      json: [],
     };
+  },
+  //mounted life-cycle hook
+  mounted() {
+    //fetches the data when the component is mounted on the dom
+    //uses fetch api
+    fetch("http://localhost:3000/jobs") // / [resource name/ object name]
+      .then((response) => {
+        response
+          .json()
+          .then((data) => (this.json = data))
+          .catch((err) => console.log(err.message));
+      });
   },
 };
 </script>
